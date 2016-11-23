d2f68a89eaa85dfb6b03ae8e0e8df94fab47aefc
Ronny Fernandes <ronnyfernandesvieira@gmail.com>
2016-11-22 23:49:01 -0200

    changes

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,31 @@
-const teste = 'testando';
-console.log(`imprimindo ${teste}`);
+import express from "express";
+import http from "http";
 
-const obj = { hey: 1, ola: 2 };
-const obj2 = { ...obj, teste: 3 };
+import { isDevelopment } from "./settings";
 
-console.log(obj2);
 
-class AppComponent {
-	static PropTypes = {
-		teste: 'ola'
-	};
-}
+// SETUP
+const app = express();
+const server = new http.Server(app);
 
-// console.log(<AppComponent />);
 
-// for (let [a] of [1, 2, 3]) {
-// 	console.log(a);
-// }
+// CONFIGURATION
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+const useExternalStyles = !isDevelopment;
+const scriptRoot = isDevelopment ? 'http://localhost:8080/build' : '/build';
+
+app.get('*', (req, res) => {
+	res.render('index', {
+		useExternalStyles,
+		scriptRoot
+	});
+});
+
+
+// STARTUP
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+	console.log(`Started http server on ${port}`);
+});
--- tests.js
+++ tests.js
@@ -1,1 +1,28 @@
-console.log('testes');
+require("source-map-support").install();
+
+const chalk = require("chalk");
+const Jasmine = require("jasmine");
+
+const tests = new Jasmine();
+tests.loadConfig({
+	spec_dir: './build/',
+	spec_files: ['**/__tests/*.js']
+});
+
+tests.configureDefaultReporter({
+	showColor: true,
+	isVerbose: true
+});
+
+tests.onComplete(function (passed) {
+	if (passed) {
+		chalk.bgGreen('All specs have passed1');
+		console.log('All specs have passed1');
+	}
+	else {
+		chalk.bgRed('At least one spec has failed1');
+		console.log('At least one spec has failed1');
+	}
+});
+
+tests.execute();
